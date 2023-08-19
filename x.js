dbca939dd917caffd1576f2469002f80108b622b
Navasalf <137118846+Navasalf@users.noreply.github.com>
2023-08-18 20:36:18 -0400

    proyecto finalizado

--- x.js
+++ x.js
@@ -1,21 +1,25 @@
 
- // Array de excusas 
- let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
- let action = ['ate', 'peed', 'crushed', 'broke'];
- let what = ['my homework', 'the keys', 'the car'];
- let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
- // Función para generar una excusa aleatoria
-function generarExcusa() 
-{ const randomwho = Math.floor(Math.random() * who.length); 
-    const randomaction = Math.floor(Math.random() * action.length); 
-    const randomwhat = Math.floor(Math.random() * what.length); 
-    const randomwhen = Math.floor(Math.random() * when.length); 
+// Array de excusas 
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+// Función para generar una excusa aleatoria
+function generarExcusa() {
+    const randomwho = Math.floor(Math.random() * who.length);
+    const randomaction = Math.floor(Math.random() * action.length);
+    const randomwhat = Math.floor(Math.random() * what.length);
+    const randomwhen = Math.floor(Math.random() * when.length);
 
 
 
-    return who [randomwho] + " " + action [randomaction] + " " + what [randomwhat] + " " + when [randomwhen]; 
+    return who[randomwho] + " " + action[randomaction] + " " + what[randomwhat] + " " + when[randomwhen];
 
 
-} 
-const excusa= generarExcusa ()
+}
+const excusa = generarExcusa()
 console.log(excusa)
+
+
+
+document.getElementById("Excuses").innerHTML = generarExcusa();
