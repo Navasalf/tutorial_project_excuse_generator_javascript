5dc1dd17697254964c84db48482cb64b25208897
Navasalf <137118846+Navasalf@users.noreply.github.com>
2023-08-18 20:06:04 -0400

    Incicio del Proyecto

---
+++ x.js
@@ -0,0 +1,21 @@
+
+ // Array de excusas 
+ let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+ let action = ['ate', 'peed', 'crushed', 'broke'];
+ let what = ['my homework', 'the keys', 'the car'];
+ let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+ // Función para generar una excusa aleatoria
+function generarExcusa() 
+{ const randomwho = Math.floor(Math.random() * who.length); 
+    const randomaction = Math.floor(Math.random() * action.length); 
+    const randomwhat = Math.floor(Math.random() * what.length); 
+    const randomwhen = Math.floor(Math.random() * when.length); 
+
+
+
+    return who [randomwho] + " " + action [randomaction] + " " + what [randomwhat] + " " + when [randomwhen]; 
+
+
+} 
+const excusa= generarExcusa ()
+console.log(excusa)
